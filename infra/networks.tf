9a5a99689fb1fbed6edf8b75950f3317a3bfcb14
Roger Bertan <roger.bertan@gmail.com>
2026-06-16 00:44:45 -0300

    feat: implement rds security group

--- infra/networks.tf
+++ infra/networks.tf
@@ -145,4 +145,32 @@ resource "aws_security_group" "ecs" {
     protocol        = "tcp"
     security_groups = [aws_security_group.load_balancer.id]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${local.project_name}-ecs-sg"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "${local.project_name}-rds-sg"
+  description = "Security group for RDS"
+  vpc_id      = aws_vpc.budgetplanner-vpc.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs.id]
+  }
+
+  tags = {
+    Name = "${local.project_name}-rds-sg"
+  }
 }
