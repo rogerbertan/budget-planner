08d5f3098e18eca7c3556ecc1a0c47d7f7113181
Roger Bertan <roger.bertan@gmail.com>
2026-06-17 23:41:53 -0300

    feat: implement listener and target group for alb

--- infra/alb.tf
+++ infra/alb.tf
@@ -8,6 +8,33 @@ resource "aws_lb" "application_load_balancer" {
   enable_deletion_protection = false
 
   tags = {
-    Name = "${local.project_name}-lb-tf"
+    Name = "${local.project_name}-lb"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.budgetplanner-tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "budgetplanner-tg" {
+  name        = "${local.project_name}-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.budgetplanner-vpc.id
+
+  health_check {
+    path                = "/actuator/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
