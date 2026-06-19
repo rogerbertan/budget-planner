50356a060c60c5c97879d8c8d773cdcf7dc7bef1
Roger Bertan <roger.bertan@gmail.com>
2026-06-19 01:17:19 -0300

    fix: add force delete to ecr

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "budgetplanner-ecr" {
   name                 = "${local.project_name}-ecr"
   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
